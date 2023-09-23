3e0d957ddd2576aaf59b3fd9331aeb3e909ae54b
shivam gupta <guptashivam 3935@gmail.com>
2023-09-23 19:09:15 +0530

    adding operation how work

--- 01_BASIC/03_conversionOperation.js
+++ 01_BASIC/03_conversionOperation.js
@@ -15,18 +15,47 @@ let isLoggedIN = "Shivam"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 let booleanIsLoggedIN = Boolean(isLoggedIN)
-console.log(booleanIsLoggedIn);
-console.log(booleanIsLoggedIN);
+// console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIN);
 
 console.log(typeof 1);
 
 let someNumber = 45
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 /**
  * 1 --> true; 0-> false
  * "" --> false
  * "Shivam"  --> true
  */
 
+//*=========================operators=====================//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+/*
+console.log(2 + 2 );
+console.log(2 - 2 );
+console.log(2 * 2 );
+console.log(2 ** 2 );
+console.log(2 / 2 );
+console.log(2 % 2 );
+*/
+
+let str1 = "Hello "
+let str2 = "Shivam"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2 );
+console.log("1" + 2 + 2 );
+console.log( 2 + "1");
+console.log( 2 + 2 + "1");
+
+console.log(true);
+console.log(+true);
+console.log("");
+console.log(+"");
