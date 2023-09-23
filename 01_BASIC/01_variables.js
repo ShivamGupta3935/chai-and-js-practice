04d5668f1f7ecd9204a84df18dee534b7f2bac81
shivam gupta <guptashivam 3935@gmail.com>
2023-09-23 15:55:23 +0530

    01_variables.js file added

---
+++ 01_BASIC/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 23455;
+let accountEmail = "shivam@gmail.com"
+var accountPassword = "535030"
+accountCity = "Mumbai"
+let accountState;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+accountState = "thane"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+accountCity="colcota"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
