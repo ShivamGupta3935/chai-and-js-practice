1303ff5668bd25f2b70414dccc24357fe6eb9e44
shivam gupta <guptashivam 3935@gmail.com>
2023-09-23 17:28:41 +0530

    conversion variable in js topic

---
+++ 01_BASIC/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score1 = 43
+let score2 = "43"
+let score3 = "32abc"
+
+// console.log(score1);
+// console.log(typeof score1);
+// console.log((typeof score1));
+
+let valueInNumber = Number(score3)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = ""
+let isLoggedIN = "Shivam"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIN = Boolean(isLoggedIN)
+console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIN);
+
+console.log(typeof 1);
+
+let someNumber = 45
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+/**
+ * 1 --> true; 0-> false
+ * "" --> false
+ * "Shivam"  --> true
+ */
+
