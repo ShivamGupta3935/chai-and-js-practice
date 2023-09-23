789826c4d17fc661e13095c56d3ebf5d17340739
shivam gupta <guptashivam 3935@gmail.com>
2023-09-23 20:30:03 +0530

    deep dive into datatype

---
+++ datatype-summary.js
@@ -0,0 +1,35 @@
+//primitive
+
+/**
+ * string, number, boolean, null, undefined, symbol, BigInt
+ */
+const myName = "Shivam"
+const score = 33
+const isLoggedIn = false
+const temperture = null
+let totalScore;
+const Biginterger = 534954257430954n
+
+console.log(typeof Biginterger);
+//reference  (Non-primitive)
+/**
+ * Array, Objects, Functions
+*/
+//Array
+const cricketPlayer = ["Dhoni", "Sachin", "Bumrah"]
+
+//
+const myDetail={
+    name: "shivam",
+    age: 23,
+    hobby:"reading",
+}
+
+
+//function
+const testFun = function(){
+    console.log("Hello Hitesh Sir How are you");
+}
+
+
+console.log(typeof testFun);
