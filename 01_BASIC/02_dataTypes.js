3937ebc74d42554a77cf894a3d3001e7b71240cc
shivam gupta <guptashivam 3935@gmail.com>
2023-09-23 16:37:25 +0530

    datatype.js file added and topic clear

---
+++ 01_BASIC/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code newer version 
+//alert("hello world")//we are using node.js not browser 
+
+console.log(3 + 
+    3)//code readability should be high 
+console.log("hello world");
+
+let name ="Shivam"
+let age = 23
+let isLoggedIn = false
+let a;
+let b = null
+
+console.log(typeof a);
+console.log(typeof b);
+
+
+// datatype
+//primitive datatype 
+//string
+//number -> 2 to power 53
+//bigInt
+//boolean -> true/false
+//null -> standalone value
+//undefined
+//symbol -> unique
+
+//Non primitive data type
+//Array
+//object
+
+
+
+
