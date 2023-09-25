2d3b40ed49be9e05e873f17012c18c180cc4b3a6
shivam gupta <guptashivam 3935@gmail.com>
2023-09-25 23:35:25 +0530

    02 basic folder added and in folder add 01 array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,26 @@
+const myArr = [1, 2, 3, 4, 5]
+// const myArry = new Array(2, 3, 5, 6, 8)
+// console.log(myArr.length);
+
+
+// myArr.push(9)
+// myArr.pop()
+// myArr.pop()
+// myArr.push(9)
+// myArr.unshift(5)
+// myArr.shift()
+
+
+console.log(myArr);
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+console.log(animals.slice(2));
+console.log(animals.slice(2,4));
+console.log(animals);
+
+console.log(animals.splice(1,3));
+console.log(animals);
+console.log(animals.length);
+
+
+
