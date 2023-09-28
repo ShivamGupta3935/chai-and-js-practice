23ed026f0dbd671b7332ee7ab9999c2b6d1a1ca5
shivam gupta <guptashivam 3935@gmail.com>
2023-09-28 21:14:18 +0530

    two.js file commited

---
+++ 06_iterations/two.js
@@ -0,0 +1,15 @@
+//while loop
+let idx = 1;
+while (idx <= 10) {
+    console.log(`value of ${idx}`);
+    idx= idx + 2
+}
+
+
+//passng array in while loop
+const heroes = ["spiderman","superman","shaktman","balveer"]
+let arr = 0
+while (arr < heroes.length) {
+    console.log(`value is ${heroes[arr]}`);
+    arr++
+}
